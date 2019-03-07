8f02a3f68a372d4735d4f7ee9a6b51a1bbdb4216
Unknown <geertex@gmail.com>
2019-03-07 15:38:46 +0100

    logging runtimes

--- src/cwts/networkanalysis/FastLocalMovingAlgorithm.java
+++ src/cwts/networkanalysis/FastLocalMovingAlgorithm.java
@@ -88,6 +88,7 @@ public class FastLocalMovingAlgorithm extends IterativeCPMClusteringAlgorithm
      */
     protected boolean improveClusteringOneIteration(Network network, Clustering clustering)
     {
+        long start = System.nanoTime();
         boolean update;
         boolean[] stableNodes;
         double maxQualityValueIncrement, qualityValueIncrement;
@@ -238,6 +239,9 @@ public class FastLocalMovingAlgorithm extends IterativeCPMClusteringAlgorithm
 
         if (update)
             clustering.removeEmptyClusters();
+
+        long end = System.nanoTime();
+        System.out.print((end-start)+",");
 
         return update;
     }
